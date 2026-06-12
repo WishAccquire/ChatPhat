b433d1ccee54e48e464435a2938a423003a9f7cc
Khushi23457 <163810647+Khushi23457@users.noreply.github.com>
2026-06-12 11:02:38 +0530

    Change SMTP transport to non-secure mode

--- utils/Mail.js
+++ utils/Mail.js
@@ -8,21 +8,20 @@ export const mail = async (email, title, body) => {
   const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: 587,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
 });
-await transporter.verify();
-console.log("SMTP verified");
+
   const info = await transporter.sendMail({
     from: `"CHATPHAT" <${process.env.MAIL_USER}>`,
     to: email,
     subject: title,
     html: body,
   });
-
+console.log("Mail sent:", info.messageId);
   return info;
 }catch (err) {
   console.error("Mail sending failed:", err);
